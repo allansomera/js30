423f008e261f3e9b20922ed87ae2d57ab91879cc
Allan Somera <nagal.somera@gmail.com>
2022-12-31 18:45:57 -0800

    finished day1 challenge, starting day2 challenge

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,15 +1,26 @@
-window.addEventListener("keydown", (e) => {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
   if (!audio) return
   audio.currentTime = 0
   audio.play()
-  console.log(key)
-  key.classList.toggle("playing")
-  const removeTransition = (e) => {
-    console.log(e)
-  }
+  // console.log(key)
+  key.classList.add("playing")
+}
 
-  const keys = document.querySelector(".key")
-  keys.forEach((key) => key.addEventListener("transitionend", removeTransition))
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return
+  // console.log(e.propertyName)
+  // this.classList.toggle("playing")
+  // console.log(this)
+  this.classList.remove("playing")
+}
+
+// const keys = document.getElementsByClassName("key")
+const keys = document.querySelectorAll(".key")
+// console.log(keys)
+keys.forEach((k) => {
+  k.addEventListener("transitionend", removeTransition)
 })
+
+window.addEventListener("keydown", playSound) ,,,
